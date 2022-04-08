4ef9034f2b2a3c12e0a373d6174d1f0efcba8134
gentleking95 <gentleking95@khu.ac.kr>
2022-04-08 10:24:01 +0900

    gugudan

---
+++ main.py
@@ -0,0 +1,14 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    for i in range(1,9):
+        for j in range(1,10):
+            print(i," * ",j ," = ",i*j)
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
